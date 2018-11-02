81e3d04fb020f78fbe6ef446ba5f215d8011d142
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-11-02 11:53:09 +0000

    core code

---
+++ src/core/Atom.py
@@ -0,0 +1,30 @@
+'''Defines the atom class
+'''
+
+
+class Atom():
+
+    def __init__(self, terms: list, name: str):
+        '''
+        Arguments:
+            terms {list} -- List of of terms defining atom
+            name {str} -- name of the predicate
+        '''
+        self._terms = terms
+        self._name = name
+
+    @property
+    def terms(self):
+        return self._terms
+
+    @listTerm.setter
+    def listTerm(self, value):
+        self._terms = value
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
